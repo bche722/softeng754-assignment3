b73317ab55d8e607bf2ff1c06e1aaac1cc8f1419
Vincent <bche722@aucklanduni.ac.nz>
2019-04-05 18:22:26 +1300

    add Dollar class and test class

---
+++ src/test/java/DollarTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.bson.Document;
+import org.junit.Test;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DollarTest {
+	@Test
+	public void testDollarDivision() {
+		Dollar ten = new Dollar(10);
+		Dollar result = ten.dividedBy(2);
+		assertEquals(new Dollar(5), result); 
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void testDollarDividedByZero() {
+		Dollar ten = new Dollar(10);
+		ten.dividedBy(0);
+	}
+}

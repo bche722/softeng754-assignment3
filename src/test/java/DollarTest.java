f20b918e9d18b57f38520460f20280d65c946b03
Vincent <bche722@aucklanduni.ac.nz>
2019-04-08 15:40:10 +1200

    test failed

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -10,12 +10,30 @@ public class DollarTest {
 	public void testDollarDivision() {
 		Dollar ten = new Dollar(10);
 		Dollar result = ten.dividedBy(2);
-		assertEquals(new Dollar(5), result); //
+		assertEquals(new Dollar(4), result); //
 	}
 
 	@Test(expected = ArithmeticException.class)
 	public void testDollarDividedByZero() {
 		Dollar ten = new Dollar(10);
 		ten.dividedBy(0);
+	}
+	
+	@Test
+	public void testDollarPersistence() {
+	/* Initialise the database connection. */
+	MongoClient mongoClient = new MongoClient("localhost", 27017);
+	String myCashDatabaseName = "my-cash-db";
+	MongoDatabase mongoDatabase =
+	mongoClient.getDatabase(myCashDatabaseName);
+	String myCashCollectionName = "my-cash-collection";
+	MongoCollection<Document> mongoCollection =
+	mongoDatabase.getCollection(myCashCollectionName);
+	/* Save the data to the database.*/
+	Dollar five = new Dollar(5);
+	Document document = new Document();
+	document.put("NZD", five.toString());
+	mongoCollection.insertOne(document);
+	System.out.println("Dollar save action performed!");
 	}
 }

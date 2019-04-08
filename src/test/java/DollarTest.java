0b5621acbb1ecd06e679d0f12da3ca884515b330
Vincent <bche722@aucklanduni.ac.nz>
2019-04-08 16:10:47 +1200

    test failed

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -18,22 +18,5 @@ public class DollarTest {
 		Dollar ten = new Dollar(10);
 		ten.dividedBy(0);
 	}
-	
-	@Test
-	public void testDollarPersistence() {
-	/* Initialise the database connection. */
-	MongoClient mongoClient = new MongoClient("localhost", 27017);
-	String myCashDatabaseName = "my-cash-db";
-	MongoDatabase mongoDatabase =
-	mongoClient.getDatabase(myCashDatabaseName);
-	String myCashCollectionName = "my-cash-collection";
-	MongoCollection<Document> mongoCollection =
-	mongoDatabase.getCollection(myCashCollectionName);
-	/* Save the data to the database.*/
-	Dollar five = new Dollar(5);
-	Document document = new Document();
-	document.put("NZD", five.toString());
-	mongoCollection.insertOne(document);
-	System.out.println("Dollar save action performed!");
-	}
+
 }
